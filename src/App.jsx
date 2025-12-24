652776e8a593282a5cf0c218d803ca0140b73ebe
anandg <anandgweb9@gmail.com>
2025-12-24 13:39:03 +0530

    Initial React + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-400">
+        GIGENTA Global
+      </h1>
+      
+      <h2 className="service">Educational Services</h2>
+      
+      <p className="text-white text-center w-[225px] mt-[30px] p-[12px] opacity-65">
+          Gigenta Global is an educational service platform created with the aim of making education easy and equitable for everyone. Our primary goal is to help students, teachers, and educational institutions grow effectively through the use of technology.
+      </p>
+      
+      <button className="text-green-400 bg-blue-800 mt-[30px] px-8 py-1 rounded-lg">Go...</button>
+    </div>
+  )
+}
