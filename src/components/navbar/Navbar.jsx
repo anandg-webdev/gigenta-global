8708a55a0327056f5a6bfca0d996da104b23d5bb
anandg <anandgweb9@gmail.com>
2025-12-30 18:44:12 +0530

    nav ui update 2812250559

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,42 +2,100 @@ import {
     TiThMenuOutline
 } from "react-icons/ti";
 
+import {
+    ImCross
+} from "react-icons/im";
+
+import {
+    useState
+} from 'react';
+
 
 import './Navbar.css';
 
 export default function Navbar() {
+
+    const [menu,
+        setMenu] = useState(false);
+
+    const showMenu = <TiThMenuOutline />
+    const hideMenu = <ImCross />
+
+    const [menuOption,
+        setMenuOption] = useState(showMenu);
+
+    const handleMenu = () => {
+        !menu?setMenuOption(hideMenu): setMenuOption(showMenu);
+
+
+
+
+
+        setMenu(!menu);
+    }
+
     return(<>
         <nav className='navbar'>
             <div className='nav-cont'>
-                <div className='nav-title-cont'>
-                    <div className='nav-title-first-letter-box'>
-                        <h1 className='nav-title-first-letter'>G</h1>
-                    </div>
-                    <div className='nav-title-second-word-boxes'>
-                        <div className='nav-title-second-word-box1'>
-                            <h4>IGENTA</h4>
+
+                <div className='nav-title-and-menu-icon'>
+
+                    <div className='nav-title-cont'>
+
+                        <div className='nav-title-first-letter-box'>
+                            <h1 className='nav-title-first-letter'>G</h1>
                         </div>
-                        <div className='nav-title-second-word-box2'>
-                            <h4>LOBAL</h4>
+
+                        <div className='nav-title-second-word-boxes'>
+                            <div className='nav-title-second-word-box1 nav-title-second-word-box'>
+                                <h4>IGENTA</h4>
+                            </div>
+                            <div className='nav-title-second-word-box2 nav-title-second-word-box'>
+                                <h4>LOBAL</h4>
+                            </div>
                         </div>
+
                     </div>
+
+
+
+
+
+                    <div className='nav-menu-cont'>
+
+                        <button className='nav-menu-icon' onClick={handleMenu}>
+                            {menuOption}
+                        </button>
+
+                    </div>
+
+
+
+
                 </div>
 
-                <div className='nav-menu-cont'>
-                    
-                    <button className='nav-menu-icon'>
-                        <TiThMenuOutline />
-                    </button>
-                    <ul className='nav-menus'>
 
+                <div className='nav-menu-list'>
+
+                    <ul className={`nav-menus ${!menu?'show': ''}`}>
+
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Services</li>
+                        <li>Solutions</li>
+                        <li>Institutions</li>
+                        <li>Students</li>
+                        <li>Teachers</li>
+                        <li>Contact Us</li>
                     </ul>
+
+
                 </div>
-
-
 
             </div>
 
         </nav>
+
     </>
     )
 }
